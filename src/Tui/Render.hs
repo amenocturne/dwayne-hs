c94dc9b4d09f71b67d1c9c29d35cab7cc581d7c2
skril <you@example.com>
2025-05-18 16:05:43 +0300

    fix layout

--- src/Tui/Render.hs
+++ src/Tui/Render.hs
@@ -3,21 +3,15 @@
 module Tui.Render where
 
 import Brick
-import Brick.Widgets.Center
 import Control.Lens
-import qualified Data.Text as T
 import Graphics.Vty.Attributes
 import Render.Render
 import qualified Render.Render as R
 import Tui.Types
 
 import Brick.Widgets.Border (vBorder)
-import Brick.Widgets.Border.Style (unicodeRounded)
 import Brick.Widgets.Dialog (renderDialog)
 import qualified Data.Vector as V
-
-ui :: String -> Widget Name
-ui text = vBox [hCenter $ str "Top widget", center $ txtWrap (T.pack text)]
 
 highlightAttr :: AttrName
 highlightAttr = attrName "highlight"
@@ -38,7 +32,13 @@ drawUI ctx =
 
 drawCompactListView :: (RenderTask a Name) => AppContext a -> [Widget Name]
 drawCompactListView ctx =
-  [joinBorders $ withBorderStyle unicodeRounded $ hBox [hLimitPercent 50 $ reportExtent CompactViewWidget compactTasks, hLimit 1 $ fill ' ', vBorder, hLimitPercent 50 maybeFocusedTask]]
+  [ hBox
+      [ padRight Max $ hLimitPercent 50 $ reportExtent CompactViewWidget compactTasks
+      , hLimit 1 $ fill ' '
+      , vBorder
+      , padRight Max $ hLimitPercent 50 maybeFocusedTask
+      ]
+  ]
  where
   compView = view compactViewLens ctx
   start = view compactViewTaskStartIndex compView
