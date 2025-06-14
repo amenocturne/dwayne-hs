ef9b1213e0193d8a417430a73feef9d8fea8c08e
skril <you@example.com>
2025-06-14 23:41:32 +0300

    fix wrapping

--- src/Render/OrgRender.hs
+++ src/Render/OrgRender.hs
@@ -8,8 +8,8 @@ module Render.OrgRender where
 import Brick
 import Control.Lens (view)
 import Data.Char (ord)
-import Data.Maybe
 import qualified Data.Map.Strict as M
+import Data.Maybe
 import qualified Data.Text as T
 import Data.Time (defaultTimeLocale)
 import Data.Time.Format (formatTime)
@@ -18,7 +18,7 @@ import Graphics.Vty.Attributes (defAttr)
 import Model.Injection
 import Model.OrgMode
 import Render.Render
-import Tui.ColorScheme (ColorScheme, levelAttr, todoKeywordAttr, priorityAttr, tagAttr, timeFieldAttr, propertyAttr, descriptionAttr)
+import Tui.ColorScheme (ColorScheme, descriptionAttr, levelAttr, priorityAttr, propertyAttr, tagAttr, timeFieldAttr, todoKeywordAttr)
 
 -- TODO: factor out common functions from it and Writer
 instance RenderTask Task b where
@@ -42,18 +42,19 @@ instance RenderTask Task b where
     renderTags [] = Nothing
     renderTags ts = Just $ T.concat [":", T.intercalate ":" ts, ":"]
 
-  renderCompactWithColors scheme task = 
-    hBox $ catMaybes
-      [ Just $ withAttr (levelAttr (view level task)) $ txt $ T.replicate (view level task) "*"
-      , Just $ txt " "
-      , Just $ withAttr (todoKeywordAttr (view todoKeyword task)) $ txt $ view todoKeyword task
-      , Just $ txt " "
-      , case view priority task of
-          Just p -> fmap (withAttr (priorityAttr p)) (renderPriorityWidget p)
-          Nothing -> Nothing
-      , Just $ txt $ view title task
-      , fmap (withAttr tagAttr) (renderTagsWidget (view tags task))
-      ]
+  renderCompactWithColors scheme task =
+    hBox $
+      catMaybes
+        [ Just $ withAttr (levelAttr (view level task)) $ txt $ T.replicate (view level task) "*"
+        , Just $ txt " "
+        , Just $ withAttr (todoKeywordAttr (view todoKeyword task)) $ txt $ view todoKeyword task
+        , Just $ txt " "
+        , case view priority task of
+            Just p -> fmap (withAttr (priorityAttr p)) (renderPriorityWidget p)
+            Nothing -> Nothing
+        , Just $ txt $ view title task
+        , fmap (withAttr tagAttr) (renderTagsWidget (view tags task))
+        ]
    where
     renderPriorityWidget p
       | p >= 0 = Just $ txt $ T.concat ["[#", T.singleton (chr $ ord 'A' + p), "] "]
@@ -71,7 +72,7 @@ instance RenderTask Task b where
         , Just propertiesSection
         , Just $ txt orgPropertiesEnd
         , Just $ txt "\n"
-        , Just $ txt $ view description task
+        , Just $ txtWrap $ view description task
         ]
    where
     titleLine =
@@ -149,31 +150,38 @@ instance RenderTask Task b where
         ]
    where
     titleLineColored =
-      hBox $ catMaybes
-        [ Just $ withAttr (levelAttr (view level task)) $ txt $ T.replicate (view level task) "*"
-        , Just $ txt " "
-        , Just $ withAttr (todoKeywordAttr (view todoKeyword task)) $ txt $ view todoKeyword task
-        , Just $ txt " "
-        , case view priority task of
-            Just p -> fmap (withAttr (priorityAttr p)) (renderPriorityWidget p)
-            Nothing -> Nothing
-        , Just $ txt $ view title task
-        , fmap (withAttr tagAttr) (renderTagsWidget (view tags task))
-        ]
-    
-    timeFieldsLineColored =
-      withAttr timeFieldAttr $ txt $
+      txtWrap $
         T.unwords $
-          mapMaybe
-            (\(field, getTime) -> fmap (renderTimeField field) (getTime task))
-            [ (orgScheduledField, view scheduled)
-            , (orgDeadlineField, view deadline)
-            , (orgClosedField, view closed)
+          catMaybes
+            [ Just $ T.replicate (view level task) "*"
+            , Just $ view todoKeyword task
+            , view priority task >>= renderPriorityText
+            , Just $ view title task
+            , renderTagsText (view tags task)
             ]
+     where
+      renderPriorityText p
+        | p >= 0 = Just $ T.concat ["[#", T.singleton (chr $ ord 'A' + p), "]"]
+        | otherwise = Nothing
 
-    propertiesSectionColored = 
-      withAttr propertyAttr $ txt $ 
-        T.intercalate "\n" (fmap (\(key, value) -> T.concat [":", key, ": ", value]) (view properties task))
+      renderTagsText [] = Nothing
+      renderTagsText ts = Just $ T.concat [":", T.intercalate ":" ts, ":"]
+
+    timeFieldsLineColored =
+      withAttr timeFieldAttr $
+        txt $
+          T.unwords $
+            mapMaybe
+              (\(field, getTime) -> fmap (renderTimeField field) (getTime task))
+              [ (orgScheduledField, view scheduled)
+              , (orgDeadlineField, view deadline)
+              , (orgClosedField, view closed)
+              ]
+
+    propertiesSectionColored =
+      withAttr propertyAttr $
+        txt $
+          T.intercalate "\n" (fmap (\(key, value) -> T.concat [":", key, ": ", value]) (view properties task))
 
     renderTimeField :: TimeField -> OrgTime -> T.Text
     renderTimeField (TimeField n delim) (OrgTime t r d) =
