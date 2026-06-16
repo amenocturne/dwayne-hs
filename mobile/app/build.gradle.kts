0154bb26b2f6167f456b24bf9054eeb36c0404a3
amenocturne <you@example.com>
2026-06-16 12:02:17 +0300

    Release Android 0.2.3

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "com.skril.dwayne"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.2.2"
+        versionCode = 5
+        versionName = "0.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
